c727ad4bcf5c8f714e9eaa3fe1e2e5e1e532e11f
fatih <fatihsenturk@mail.com>
2016-06-05 17:00:08 +0300

    initialized intelij idea based plugin project

---
+++ src/TextBoxes.java
@@ -0,0 +1,26 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * Created by 4fath on 5.6.2016.
+ *
+ */
+public class TextBoxes extends AnAction {
+
+    public TextBoxes() {
+        // Set the menu item name.
+        super("Text _Boxes");
+        // Set the menu item name, description and icon.
+        // super("Text _Boxes","Item description",IconLoader.getIcon("/Mypackage/icon.png"));
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
